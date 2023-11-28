3ae6f12668b675394cef349494734286943304c5
Kevinsanc123 <kevinsantiagocb6@gmail.com>
2023-11-27 20:06:22 -0500

    punto-1

---
+++ Punto-1/index.php
@@ -0,0 +1,17 @@
+<?php
+//contates
+const po = "bienvenido";
+
+//variable
+$name = "kevin";
+
+//mostra en la consola
+echo po . " " . $name;
+
+//canbiar variable
+
+$name = "pc alta calidad";
+
+//mostrar en la consola
+
+  echo po . " " . $name;
