d93fe8e3be8791081f0b160f558145d6dec8186a
Kevinsanc123 <kevinsantiagocb6@gmail.com>
2023-12-01 15:55:51 -0500

    punto-4

---
+++ punto-4/index.php
@@ -0,0 +1,11 @@
+<?php
+$name = "";
+$apellido = "";
+
+echo "ingresa un nombre" . "\n";
+$name = readline();
+
+echo "ingresa un apellido ". "\n";
+  $apellido = readline();
+
+echo "tu nombre es " . $name . " " . $apellido ;
