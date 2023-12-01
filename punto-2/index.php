4770b13500f2191f43f52585bd89c5b79d2e2d84
Kevinsanc123 <kevinsantiagocb6@gmail.com>
2023-12-01 15:58:10 -0500

    punto-2

---
+++ punto-2/index.php
@@ -0,0 +1,13 @@
+<?php
+const FIRST_NUMBER = 8 ;
+$secondNumber = 4;
+
+$result = FIRST_NUMBER + $secondNumber;
+
+echo FIRST_NUMBER ." + " . $secondNumber . " = " . $result . "\n"; 
+
+$secondNumber = 6;
+
+$result = FIRST_NUMBER + $secondNumber;
+
+echo FIRST_NUMBER ." + " . $secondNumber . " = " . $result; 
