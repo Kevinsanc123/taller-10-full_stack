0b85b86e7796445d385010d272cd25c3bce8d18e
Kevinsanc123 <kevinsantiagocb6@gmail.com>
2023-12-02 17:57:03 -0500

    punto-13

---
+++ punto-13/index.php
@@ -0,0 +1,8 @@
+<?php
+$numberArray = array( 24, 86, 18, 16, 88, 54, 78, 35, 841, 35, 941,861);
+  $number = 0;
+
+foreach( $numberArray as $num)
+ $sum = $sum + $num;
+
+echo "el numero myor es " . $sum;
